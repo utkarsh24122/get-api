2d4fbe6d20d10b193a09924218f279e024c0ad51
Utkarsh Sharma <54320208+utkarsh24122@users.noreply.github.com>
2021-08-28 23:41:45 +0530

    Update getapi.sh

--- getapi.sh
+++ getapi.sh
@@ -1,19 +1,68 @@
+echo ""
+echo "                                                                               _                       _  "
+echo "                                                                     __ _  ___| |_          __ _ _ __ (_) "
+echo "                                                                    / _  |/ _ \ __|  ___   / _  | '_ \| | "
+echo "                                                                   | (_| |  __/ |_  |___| | (_| | |_) | | "
+echo "                                                                    \__, |\___|\__|        \__,_| .__/|_| "
+echo "                                                                    |___/                       |_|       "
 
 echo ""
-echo "get-api by @utkarsh24122"
+echo "                                                                             get-api by @utkarsh24122"
+
+
 target=$1
+
 echo ""
+echo " "
 echo "input file ===> $target"
 echo ""
 
 echo $target | cut -d . -f1 >> log.txt
 read -r foo<log.txt ; rm -f log.txt
+
 mkdir "$foo"_api
+
 cp "$target" ./"$foo"_api/"$target"
+
 cd "$foo"_api
-wget -q https://raw.githubusercontent.com/panch0r3d/nuclei-templates/1a1e3a8b8f6c45cfadcfff1078287339dd950896/api_endpoints.yaml 
+
 pwd >> log.txt
-read -r foo2<log.txt ; rm -f log.txt
-cat "$target" | nuclei -silent -t "$foo2" -o temp.txt
-cat temp.txt | sort -u >> api-endpoints.txt ; rm -f temp.txt
-rm *.yaml
+read -r foo2<log.txt ;rm -f log.txt
+
+
+echo ""
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Output directory : "$foo2" \e[0m\n" 
+echo ""
+
+
+
+echo ""
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Getting template ... \e[0m\n" 
+echo ""
+wget -q https://raw.githubusercontent.com/panch0r3d/nuclei-templates/1a1e3a8b8f6c45cfadcfff1078287339dd950896/api_endpoints.yaml 
+echo "api_endpoints.yaml                             100%[====================================================================================================>]   2.09K  --.-KB/s "
+echo ""
+
+
+echo ""
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Finding API Endpoints ... \e[0m\n" 
+cat $target | httpx -silent | nuclei -silent -t api_endpoints.yaml -o apiget.txt
+echo ""
+
+
+echo ""
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Extracting links and sorting ... \e[0m\n" 
+echo ""
+echo ""
+
+cat apiget.txt | cut -d " " -f6 | sort -u >> api-endpoints.txt
+rm apiget.txt
+
+cat api-endpoints.txt | wc -l >> log.txt
+read -r foo3<log.txt ;rm -f log.txt
+
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m "$foo3" API Endpoints found and saved in "$foo2"/api-endpoints.txt \e[0m\n" 
+echo ""
+echo ""
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Scan Complete!!! Exiting ... \e[0m\n" 
+echo ""
