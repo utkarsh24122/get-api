af88b8191794892328714d27ed88a120902d6a6b
Utkarsh Sharma <54320208+utkarsh24122@users.noreply.github.com>
2021-08-28 14:57:13 +0530

    Update getapi.sh

--- getapi.sh
+++ getapi.sh
@@ -1,12 +1,19 @@
+
 echo ""
 echo "get-api by @utkarsh24122"
 target=$1
 echo ""
 echo "input file ===> $target"
-echo "" 
-echo $target | cut -d . -f1 | read foo
-echo $foo
-echo "All Output stored in /"$target"_s3 "
-echo "" 
-mkdir $target_s3
-cd $taget_s3
+echo ""
+
+echo $target | cut -d . -f1 >> log.txt
+read -r foo<log.txt ; rm -f log.txt
+mkdir "$foo"_api
+cp "$target" ./"$foo"_api/"$target"
+cd "$foo"_api
+wget -q https://raw.githubusercontent.com/panch0r3d/nuclei-templates/1a1e3a8b8f6c45cfadcfff1078287339dd950896/api_endpoints.yaml 
+pwd >> log.txt
+read -r foo2<log.txt ; rm -f log.txt
+cat "$target" | nuclei -silent -t "$foo2" -o temp.txt
+cat temp.txt | sort -u >> api-endpoints.txt ; rm -f temp.txt
+rm *.yaml
